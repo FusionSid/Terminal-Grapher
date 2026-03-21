a06c07f90860ca6f7dac75a68cd1dc2b764c84ff
FusionSid <siddheshadsv@icloud.com>
2026-03-22 00:28:32 +1300

    feat: common ansi escape sequences

---
+++ codes.h
@@ -0,0 +1,8 @@
+#define ENABLE_ALT_BUFFER "\x1b[?1049h"
+#define DISABLE_ALT_BUFFER "\x1b[?1049l"
+#define HIDE_CURSOR "\x1b[?25l"
+#define SHOW_CURSOR "\x1b[?25h"
+#define START_NEXT_LINE "\x1b[1E"
+#define ERASE_SCREEN "\x1b[2J"
+#define DISABLE_LINE_WRAP "\x1b[7l"
+#define ENABLE_LINE_WRAP "\x1b[7h"
